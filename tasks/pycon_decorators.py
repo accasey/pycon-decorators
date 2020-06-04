cd609737000292b7262b14ed646621737537650f
Andrew Casey <accasey@hotmail.com.au>
2020-06-04 19:37:49 +0100

    Finished task 2. Now at 1:15:00

--- tasks/pycon_decorators.py
+++ tasks/pycon_decorators.py
@@ -31,14 +31,47 @@ def timer(func):
 
 
 def trace(func):
-    """Template for decorators"""
+    """Show the trace of function calls"""
+
+    # This only happens once, instead of every time
+    name = func.__name__
+
+    @functools.wraps(func)
+    def _trace(*args, **kwargs):
+        """The trace function replacing the original function"""
+        args_repr = [repr(a) for a in args]
+        # kwargs_repr = [f"{k}={repr(v)}" for k, v in kwargs.items()]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()] # this !r is a shortcut for repr()
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {name}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{name} returned {value!r}")
+        return value
+
+    return _trace
+
+
+def my_orig_trace(func):
+    """Show the trace of function calls"""
 
     @functools.wraps(func)
     def _wrapper(*args, **kwargs):
         """The wrapper function replacing the original"""
-        # Do something before calling the function
+        args_str = ""
+        for arg in args:
+            if isinstance(arg, str):
+                args_str += "'" + arg + "', "
+            else:
+                args_str += str(arg) + ", "
+
+        for k, v in kwargs.items():
+            args_str += str(k) + f"='{v}', "
+
+        args_str = args_str.rstrip(", ")
+        # args_str = args_str.rstrip(',')
+        print(f"Calling {func.__name__}({args_str})")
         value = func(*args, **kwargs)
-        # Do something after calling the function
+        print(f"{func.__name__} returned '{value}'")
         return value
 
     return _wrapper
--- tasks/task02.py
+++ tasks/task02.py
@@ -1,9 +1,9 @@
 import random
 
-from pycon_decorators import trace
+from pycon_decorators import trace, timer
 
 
-GREETINGS = ["Heisann", "Hi there", "Ni!"]
+GREETINGS = ["Heisann", "Hi there", "Ni!", "Wassup", "Naber", "Merhaba"]
 
 
 @trace
@@ -11,6 +11,7 @@ def greet(name, greeting="Hello"):
     return f"{greeting} {name}"
 
 
+@timer
 @trace
 def random_greet(name="Emily"):
     greeting = random.choice(GREETINGS)
@@ -20,3 +21,19 @@ def random_greet(name="Emily"):
 @trace
 def greet_many(number):
     return [random_greet() for _ in range(number)]
+
+
+if __name__ == "__main__":
+    # greet("Andrew")
+    # greet("Andrew", greeting="Naber")
+    # greet(name="Andrew", greeting="Naber")
+    # random_greet()
+    # greet_many(3)
+    random.seed(2020)
+    greet("world")
+    print("-" * 50)
+    greet(name="world", greeting="def")
+    print("-" * 50)
+    random_greet()
+    print("-" * 50)
+    greet_many(3)
